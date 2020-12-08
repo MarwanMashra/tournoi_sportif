2af2d55148bf4cc53cc5a29483e15f5780c94ea6
Marwan Mashra <47461193+MarwanMashra@users.noreply.github.com>
2020-12-08 01:09:12 +0100

    Update page_home.php

--- page_home.php
+++ page_home.php
@@ -10,6 +10,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>projet</title>
 	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="daterangepicker.css" rel="stylesheet" type="text/css" />
 
 </head>
 <body>
@@ -24,7 +26,9 @@
 			}
 		?>
 	  
-	  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-	  <script src="home.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+	  	<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+      	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+	  	<script src="home.js"></script>
 </body>
 </html>
