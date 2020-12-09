46de31e93a494f5bf7d815c427f5772bb54877be
root <anh.cao@etu.umontpellier.fr>
2020-12-09 16:14:09 +0100

    ajout_Equipe

--- formulaire_equipe.php
+++ formulaire_equipe.php
@@ -1,3 +1,15 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+session_start();
+if(! isset($_SESSION['login'])){
+	header('Location:page_connexion.php');
+}
+if(isset($_SESSION['message']))
+{
+	$message= $_SESSION['message'];
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +18,7 @@
 	<meta charset="utf-8">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="auth.css">
+	<!-- <link rel="stylesheet" type="text/css" href="auth.css"> -->
 	<!-- <link rel="icon" href="{{url_for('static', filename = 'src/icons/home.png')}}" /> -->
 
 </head>
@@ -15,6 +27,12 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="form_equipe.js"></script>
+	<?php
+	if(isset($message)){
+		echo "<br><span class='${message['class']}'>*${message['text']}</span>";
+	}
+	?>
+	
 
 </body>
 </html>
